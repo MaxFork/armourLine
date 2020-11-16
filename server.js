bb37a5c04a77e1bf48a3435bd9bc44898f8ccf73
Gurkiran Singh <gurkiransinghk@gmail.com>
2020-11-16 09:46:49 -0500

    Part 7

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build")));
 
-    // app.get("*", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "client/build", "index.html"))
-    // })
+    app.get("*", function(request, response) {
+        response.sendFile(path.join(__dirname, "client/build", "index.html"))
+    });
 }
 
 //port
