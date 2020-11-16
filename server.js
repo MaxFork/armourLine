5dfc8f17820eb1663ca8bef70669156d4a86ae03
Gurkiran Singh <gurkiransinghk@gmail.com>
2020-11-15 19:40:50 -0500

    testing backend and frontend connection in production part 2

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
-require('dotenv').config()
 const express = require('express');
 const nodemailer = require("nodemailer");
 const smtpTransport = require("nodemailer-smtp-transport");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const helmet = require("helmet");
 const compression = require("compression");
 
+if(process.env.NODE_ENV !== "production") require('dotenv').config();
+
 const app = express();
-
-
-// serve the react app files
-app.use(express.static(`${__dirname}/client/build`));
-
 
 //middlewares
 app.use(helmet());
 app.use(compression());
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// serve the react app files in production
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client/build", "index.html"))
+    })
+}
 
 //port
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Backend is running on localhost:${port}`));
+app.listen(port, error => {
+    if(error) throw error;
+    console.log(`Backend is running on localhost:${port}`);
+});
 
 // connection route
 app.get("/express", (req, res) => res.json({express: "YOUR EXPRESS BACKEND IS CONNECTED"}));
