57ebfc919eb4902a3b34ec4fabdbe0aed6d2b4ff
Gurkiran Singh <gurkiransinghk@gmail.com>
2020-11-16 12:52:34 -0500

    Part 8

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build")));
 
-    app.get("*", function(request, response) {
-        response.sendFile(path.join(__dirname, "client/build", "index.html"))
-    });
+    app.get("/express", (req, res) => res.json({express: "YOUR EXPRESS BACKEND IS CONNECTED"}));
 }
 
 //port
